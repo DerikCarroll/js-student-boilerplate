c1ee5edfc39f7c7d53c1f859f189dbd082f541ab
DerikCarroll <derikcarroll@gmail.com>
2019-08-31 20:45:00 -0500

    Update scrips.js with comments

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 // Write a program that sums the numbers from 1 to 10.
 
-let counter = 1;
-let sum = 0;
+let counter = 1; // Counter for while loop starting at 1
+let sum = 0; // Sum of the numbers
 
-// `<=` is  COMPARISON OPERATOR
+// `<=` is a comparison operator that means less than or equal to
 while (counter <= 10) {
   sum += counter; // sum = sum + counter;
 
-  // `+=` is an ASSIGNMENT OPERATOR
+  // `+=` is an assignment operator that adds and then assigns the new value
   counter += 1; // counter = counter + 1;
 }
 
-console.log(sum);
+console.log(sum); // Displays the sum in the console
