fe323a54d9cb65d9764f0ad358c00b3b5b3b29f9
DerikCarroll <derikcarroll@gmail.com>
2019-09-19 21:37:06 -0500

    Replaced 'while' loop with a 'for' loop

--- scripts.js
+++ scripts.js
@@ -1,14 +1,11 @@
 // Write a program that sums the numbers from 1 to 10.
-
-let counter = 1; // Counter for while loop starting at 1
 let sum = 0; // Sum of the numbers
 
 // `<=` is a comparison operator that means less than or equal to
-while (counter <= 10) {
-  sum += counter; // sum = sum + counter;
-
-  // `+=` is an assignment operator that adds and then assigns the new value
-  counter += 1; // counter = counter + 1;
+// For loop declares, compares, and increments its own variable for the loop
+for (let i = 0; i <= 10; i += 1){
+    // `+=` is an assignment operator that adds and then assigns the new value
+  sum += i; // sum = sum + i;
 }
 
 console.log(sum); // Displays the sum variable in the console
