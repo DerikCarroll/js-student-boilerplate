9ea57c2cdf1820a5b5f70b545053ea27c8b69667
DerikCarroll <derikcarroll@gmail.com>
2019-09-19 21:59:53 -0500

    Add numberSummer function.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,18 @@
-// Write a program that sums the numbers from 1 to 10.
-let sum = 0; // Sum of the numbers
+// Write a program that sums the numbers from any given starting point and any given ending point.
 
-// `<=` is a comparison operator that means less than or equal to
-// For loop declares, compares, and increments its own variable for the loop
-for (let i = 0; i <= 10; i += 1){
+// A function can accepts named parameters.
+function numberSummer(start, end) {
+  let sum = 0; // Sum of the numbers.
+
+  // `<=` is a comparison operator that means less than or equal to
+  // For loop declares, compares, and increments its own variable for the loop
+  for (let i = start; i <= end; i += 1){
     // `+=` is an assignment operator that adds and then assigns the new value
-  sum += i; // sum = sum + i;
+    sum += i; // sum = sum + i;
+  }
+
+  console.log(sum); // Displays the sum variable in the console
 }
 
-console.log(sum); // Displays the sum variable in the console
+// Invoke the function and pass in arguments.
+numberSummer(1, 10);
