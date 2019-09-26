6e394bf01616aaac540575dc2a6fb7ea0fc2df12
DerikCarroll <derikcarroll@gmail.com>
2019-09-25 23:01:47 -0500

    Fixed script tag in index.html. Add output of total from function to output tag.

--- scripts.js
+++ scripts.js
@@ -2,12 +2,12 @@
 
 // A function can accept named parameters.
 // Default variables assigned if no arguments passed in.
-function numberSummer(start = 1, end = 10) {
+function numberSummer(start = 1, end = 10, inc = 1) {
   let sum = 0; // Sum of the numbers.
 
   // `<=` is a comparison operator that means less than or equal to
   // For loop declares, compares, and increments its own variable for the loop
-  for (let i = start; i <= end; i += 1){
+  for (let i = start; i <= end; i += inc) {
     // `+=` is an assignment operator that adds and then assigns the new value
     sum += i; // sum = sum + i;
   }
@@ -17,5 +17,9 @@ function numberSummer(start = 1, end = 10) {
 
 // Invoke the function and pass in arguments.
 // Assigns the total variable the value of the sum variable.
-const total = numberSummer(1, 10);
-console.log(total);
+const total = numberSummer(5, 500, 5);
+
+// Assign the output from the for loop to a new variable.
+const output = document.querySelector("output");
+// Set the output tag to the total calculated by the for loop.
+output.textContent = total;
